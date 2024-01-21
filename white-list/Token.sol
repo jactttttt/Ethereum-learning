cb6fc672848f35f858c76829b0d2e230b4d35deb
jactttttt <111186732+jactttttt@users.noreply.github.com>
2024-01-22 00:53:26 +0800

    Update Token.sol

--- white-list/Token.sol
+++ white-list/Token.sol
@@ -1,1 +1,24 @@
 // SPDX-License-Identifier: MIT
+
+pragma solidity  ^0.8.20;
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+
+    address public factory;
+
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+        factory = msg.sender;
+    }
+
+    function mint(address to, uint amount) external {
+        require(msg.sender == factory, "Only factory mint");
+        _mint(to, amount);
+    } 
+
+    function burn(uint amount) external {
+        require(msg.sender == factory, "Only factory burn");
+        _burn(msg.sender, amount);
+    }
+}
